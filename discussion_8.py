b4e15c13c3d27aaf4cfc3e90b8077f33278a87aa
no <81531433+monoallana@users.noreply.github.com>
2022-10-26 14:22:44 -0400

    thankyou!

--- discussion_8.py
+++ discussion_8.py
@@ -5,52 +5,75 @@ import unittest
 # Task 2: Look at the Get the URL that links to webpage of universities with Olympic medal wins
 # search for the url in the University of Michgian wikipedia page (in the third pargraph of the intro)
 # HINT: You will have to add https://en.wikipedia.org to the URL retrieved using BeautifulSoup
+
+
 def getLink(soup):
-    
-    pass
+    # get the a tag with title "List of American universities with Olympic medals"
+    a_tag = soup.find(
+        "a", title="List of American universities with Olympic medals")
+    url = a_tag.get('href')
+    print('https://en.wikipedia.org' + url)
+    return 'https://en.wikipedia.org' + url
 
 # Task 3: Get the details from the box titled "College/school founding". Get all the college/school names and the year they were
 # founded and organize the same into key-value pairs.
+
+
 def getAdmissionsInfo2019(soup):
+    table_tag = soup.find("table", class_='toccolours')
+    tr_list = table_tag.find_all('tr')
+    d = {}
 
-    pass
+    for tr in tr_list[1:]:
+        td_list = tr.find_all("td")
+        school_name = td_list[0].text.strip()
+        year = td_list[1].text.strip()
+        d[school_name] = year
 
+    print(d)
+    return d
 
 
 def main():
     # Task 1: Create a BeautifulSoup object and name it soup. Refer to discussion slides or lecture slides to complete this
-
     #### YOUR CODE HERE####
-
-    #Call the functions getLink(soup) and getAdmissionsInfo2019(soup) on your soup object.
+    url = 'https://en.wikipedia.org/wiki/University_of_Michigan'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
     getLink(soup)
     getAdmissionsInfo2019(soup)
 
+    # Call the functions getLink(soup) and getAdmissionsInfo2019(soup) on your soup object.
+
+
 class TestAllMethods(unittest.TestCase):
     def setUp(self):
-        self.soup = BeautifulSoup(requests.get('https://en.wikipedia.org/wiki/University_of_Michigan').text, 'html.parser')
+        self.soup = BeautifulSoup(requests.get(
+            'https://en.wikipedia.org/wiki/University_of_Michigan').text, 'html.parser')
 
     def test_link_nobel_laureates(self):
-        self.assertEqual(getLink(self.soup), 'https://en.wikipedia.org/wiki/List_of_American_universities_with_Olympic_medals')
+        self.assertEqual(getLink(
+            self.soup), 'https://en.wikipedia.org/wiki/List_of_American_universities_with_Olympic_medals')
 
     def test_admissions_info(self):
-        self.assertEqual(getAdmissionsInfo2019(self.soup), {'Engineering': '1854', 
+        self.assertEqual(getAdmissionsInfo2019(self.soup), {'Engineering': '1854',
                                                             'Law': '1859',
-                                                            'Dentistry': '1875', 
-                                                            'Pharmacy': '1876', 
-                                                            'Music, Theatre &Dance': '1880', 
-                                                            'Nursing': '1893', 
-                                                            'Architecture &Urban Planning': '1906', 
-                                                            'Graduate Studies': '1912', 
-                                                            'Government': '1914', 'Education': 
-                                                            '1921', 'Business': '1924', 
-                                                            'Environment andSustainability': '1927', 
-                                                            'Public Health': '1941', 
-                                                            'Social Work': '1951', 
-                                                            'Information': '1969', 
-                                                            'Art & Design': '1974', 
+                                                            'Dentistry': '1875',
+                                                            'Pharmacy': '1876',
+                                                            'Music, Theatre &Dance': '1880',
+                                                            'Nursing': '1893',
+                                                            'Architecture &Urban Planning': '1906',
+                                                            'Graduate Studies': '1912',
+                                                            'Government': '1914', 'Education':
+                                                            '1921', 'Business': '1924',
+                                                            'Environment andSustainability': '1927',
+                                                            'Public Health': '1941',
+                                                            'Social Work': '1951',
+                                                            'Information': '1969',
+                                                            'Art & Design': '1974',
                                                             'Kinesiology': '1984'})
+
 
 if __name__ == "__main__":
     main()
-    unittest.main(verbosity = 2)
+    unittest.main(verbosity=2)
